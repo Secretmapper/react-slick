9c7a796c9690ef478219aa86f0679c5f184f3f21
Arian Allenson M. Valdez <Secretmapper16@gmail.com>
2017-03-30 15:48:26 +0800

    Add slick-center regardless of center mode (fix)

--- src/track.js
+++ src/track.js
@@ -15,13 +15,15 @@ var getSlideClasses = (spec) => {
   }
 
   slickCloned = (index < 0) || (index >= spec.slideCount);
-  slickCenter = (index - spec.currentSlide) % spec.slideCount === 0;
   if (spec.centerMode) {
     centerOffset = Math.floor(spec.slidesToShow / 2);
+    slickCenter = (index - spec.currentSlide) % spec.slideCount === 0;
     if ((index > spec.currentSlide - centerOffset - 1) && (index <= spec.currentSlide + centerOffset)) {
       slickActive = true;
     }
   } else {
+    centerOffset = Math.floor(spec.slidesToShow / 2);
+    slickCenter = index === spec.currentSlide + centerOffset
     slickActive = (spec.currentSlide <= index) && (index < spec.currentSlide + spec.slidesToShow);
   }
   return classnames({
